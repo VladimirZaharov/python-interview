b36de690ee56a7c6af2f45eb4c01c1376c88afbd
Vladimir <vovazakharov2@gmail.com>
2021-12-02 11:56:02 +0300

    done

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.sites.models import Site
+from django.contrib.sites.managers import CurrentSiteManager
 
 # Create your models here.
 class MyAppModel(models.Model):
@@ -7,14 +9,17 @@ class MyAppModel(models.Model):
     price = models.IntegerField(default=0)
     unit = models.CharField(max_length=15)
     producer = models.CharField(max_length=64)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
+    objects = CurrentSiteManager('site')
 
     def __str__(self):
         return self.name
 
+
 class CategoriesModel(models.Model):
     category = models.CharField(max_length=64, default='Unknown')
-    product = models.ManyToManyField(MyAppModel, related_name='cat')
-    some_model = models.CharField(max_length=64, default='Unknown')
+    product = models.ManyToManyField(MyAppModel, related_name='product')
+    objects = models.Manager()
 
     def __str__(self):
         return self.category
